f2a2c102bad1fe0086ea96b9d8754c2e97f739bf
tifadea <abahmamat061@gmail.com>
2026-02-08 12:48:25 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Kode JavaScript untuk menampilkan pesan saat website dibuka
+alert("Selamat datang! Website HTML, CSS, dan JS ini berhasil dibuat.");
+
+// Kode untuk mencatat teks di console (untuk developer)
+console.log("Script JavaScript berjalan dengan baik.");
